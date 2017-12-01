5abbd7d4816c2811223a0d90d86d51bf60b9dcfb
Anton Lindqvist <anton.lindqvist@gmail.com>
2017-12-01 11:52:46 +0100

    Favor stdint.h over sys/types.h for uint8_t definition

--- wmidump.c
+++ wmidump.c
@@ -28,12 +28,11 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
-#include <sys/types.h>
-
 #include <ctype.h>
 #include <err.h>
 #include <errno.h>
 #include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
